02dbd02865a3d40ad1df311fa1cf1084410af6ed
Victoria Vilas Boas <vick_vilasb@outlook.com>
2019-06-11 15:38:04 -0300

    basic files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment, Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    const name = "Victória Vilas Boas";
+    return (
+      <Fragment>
+        <h1>Hello {name}</h1>
+      </Fragment>
+    );
+  }
+  
+}
+
+export default App;
